0ac9cacbfe46396e4fdab6a69d6c398d5cb4c822
osvilla27 <oscaravg27@gmail.com>
2023-03-04 12:41:21 -0500

    Linux

--- core/settings.py
+++ core/settings.py
@@ -44,8 +44,8 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = "/usr/bin/npm"
+#NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
